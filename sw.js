516b6e74a6b3d6cacc19319c5d6ba88f7e100390
Zero9923 <1325202861@qq.com>
2026-03-28 13:28:22 +0800

    Enable icon caching in service worker
    Uncomment icon file paths for caching in service worker.

---
+++ sw.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = 'Rainy';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  // 如果你准备了图标，把下面两行取消注释
+  // './icon-192.png',
+  // './icon-512.png'
+];
+
+// 安装 Service Worker 并缓存核心文件
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// 拦截网络请求，优先使用缓存
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // 如果在缓存中找到，直接返回缓存；否则发起网络请求
+        return response || fetch(event.request);
+      })
+  );
+});
+
+// 更新缓存
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
